61a7e6ccedce9d484f834cf22dd84695936a4576
Rioske-Nakamura <rioskenakamura@gmail.com>
2023-05-23 21:58:40 -0400

    Transferi dados do js para o html em forma de cartao
    nunca se esqueça do ${ }

--- script.js
+++ script.js
@@ -9,7 +9,22 @@ class Produto{
     };
 
     emprimir(){
-        return this.Nome + this.Data_de_Cadastro + this.Descricao + this.Preco
+        
+        return `
+        <div class="card-header">
+        ${this.Nome}
+      </div>
+      <blockquote class="blockquote mb-0">
+      <p> ${this.Descricao} </p>
+      <footer class="blockquote-footer"> ${this.Data_de_Cadastro}</footer>
+      <p> ${'R$ ' + this.Preco} </p>
+    </blockquote>
+  </div>
+
+       
+        
+        
+        `
     }
 
 };
@@ -19,10 +34,32 @@ class ProdutosDestaque extends Produto{
     constructor(Nome, Data_de_Cadastro, Descricao,Preco, imagem){
         super(Nome, Data_de_Cadastro, Descricao,Preco),
         this.imagem = imagem
+       
     }
 
     mostrardestaque(){
-        return this.Nome + this.Data_de_Cadastro + this.Descricao + this.Preco  + this.imagem
+        return `
+        <img src=${this.imagem} class="card-img-top">  </img>
+        <div class="card-body">
+        <h1 class="card-title"> ${this.Nome} </h1>
+        <p class="card-text"> ${this.Data_de_Cadastro} </p>
+        <p class="card-text"> ${this.Descricao} </p>
+        <h3 class="card-text"> ${'R$ '+this.Preco} </h3>
+        </div>`
+        
+        
+        //  
     }
 }
 
+let produtos= new Produto("BelaAna", "2009-01-09", "Produto de Baixa Qualidade", 1.99)
+let div= document.getElementById("lista-produtos")
+
+div.insertAdjacentHTML('afterbegin', produtos.emprimir())
+
+//Destacados
+let produtodestaque= new ProdutosDestaque("Vall","2019-02-22","Uma variante bem boazinha", 50, "kaka.jpg")
+let divD = document.getElementById("produto-destaque")
+
+console.log(produtodestaque.mostrardestaque())
+divD.insertAdjacentHTML('afterbegin', produtodestaque.mostrardestaque())
